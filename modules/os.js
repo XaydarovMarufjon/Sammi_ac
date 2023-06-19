6c11953e251736cc30e92d18fb49a89b2dc85845
Ibrohim <aigimtia@gmail.com>
2023-06-19 01:09:40 -0400

    addes files

---
+++ modules/os.js
@@ -0,0 +1,10 @@
+const os = require('os');
+
+// console.log(os.platform()); // qanday os  ishlatayotganini korsatadi 
+
+// console.log(os.cpus()); /// compyuter haqida malumot 
+
+// console.log(os.arch()); // razshireniya  x64
+
+console.log(os.freemem()); /// kompyuterdagi   joy 
+console.log();  /// kompyuterdagi bosh joy 
