da1a373a08f221fd78a2774d270f71a38014bc59
Ibrohim <aigimtia@gmail.com>
2023-06-20 00:13:07 -0400

    added files

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 const http = require("http");
+const fs = require("fs");
+const path = require('path')
 
 const server = http.createServer((req , res)=>{
      // request - sorov
      // response - javob
  console.log(req.url);
 
- res.write("Helllo world") ; 
- res.end()
+ if (req.method === 'GET') {
+    res.writeHead(200 , {'Content-Type' : 'text/html'})
+  
+     if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'templates' , 'index.html'), 'utf-8' , (err, conten)=>{
+            if(err) throw new err
+            res.end(conten)
+        })
+     } else if(req.url === '/about'){
+        fs.readFile(path.join(__dirname, 'templates' , 'about.html'), 'utf-8' , (err, conten)=>{
+            if(err) throw new err
+            res.end(conten)
+        })
+     }
+     else if(req.url === '/contact'){
+        fs.readFile(path.join(__dirname, 'templates' , 'contact.html'), 'utf-8' , (err, conten)=>{
+            if(err) throw new err
+            res.end(conten)
+        })
+     }
+
+ }  else if (req.method === 'POST') {
+    const body =[]
+    res.writeHead(200 , {'Content-Type'  : 'text/html; charset=utf-8'})
+
+    req.on('data' , data =>{
+        body.push(Buffer.from(data))
+    })
+
+    req.on('end',  ()=>{
+        const message = body.toString().split('=')[1]
+        console.log(body.toString());
+        res.end(`Name was succesfuly added: ${message}`)
+    })
+ }
+
 })
 
 
